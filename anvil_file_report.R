e80a4758b44690270366224ee3102fc7b33a402b
Stephanie M. Gogarten <sdmorris@uw.edu>
2022-04-11 15:57:59 -0700

    pass in model file as an argument

--- anvil_file_report.R
+++ anvil_file_report.R
@@ -3,13 +3,11 @@ library(AnvilDataModels)
 
 argp <- arg_parser("report")
 argp <- add_argument(argp, "--table_files", help="2-column tsv file with (table name, table tsv file)")
+argp <- add_argument(argp, "--model_file", help="tsv file with data model")
 argp <- add_argument(argp, "--out_prefix", help="output prefix")
 argv <- parse_args(argp)
 
-model_file <- "https://raw.githubusercontent.com/UW-GAC/primed_data_models/main/PRIMED_data_model_draft.tsv"
-
 tables <- readr::read_tsv(argv$table_files, col_names=c("names", "files"), col_types = readr::cols())
-params <- list(tables=setNames(tables$files, tables$names), model=model_file)
+params <- list(tables=setNames(tables$files, tables$names), model=argv$model_file)
 
-custom_render_markdown("data_model_report", argv$out_prefix, 
-                       parameters=params)
+custom_render_markdown("data_model_report", argv$out_prefix, parameters=params)
