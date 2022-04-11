2e60d1544594e78c92422fa93aba7460b311217e
Stephanie M. Gogarten <sdmorris@uw.edu>
2022-04-11 15:08:13 -0700

    read table name,file pairs from tsv isntead of command line
    The WDL map type supports reading and writing from files but
    not serializing directly in the command.

--- anvil_file_report.R
+++ anvil_file_report.R
@@ -2,17 +2,14 @@ library(argparser)
 library(AnvilDataModels)
 
 argp <- arg_parser("report")
-argp <- add_argument(argp, "--table_files", nargs=Inf, help="tsv files with data tables")
-argp <- add_argument(argp, "--table_names", nargs=Inf, help="tsv file with data model")
+argp <- add_argument(argp, "--table_files", help="2-column tsv file with (table name, table tsv file)")
 argp <- add_argument(argp, "--out_prefix", help="output prefix")
 argv <- parse_args(argp)
 
-stopifnot(length(argv$table_files) == length(argv$table_names))
-
 model_file <- "https://raw.githubusercontent.com/UW-GAC/primed_data_models/main/PRIMED_data_model_draft.tsv"
 
-params <- list(tables=setNames(argv$table_files, argv$table_names),
-               model=model_file)
+tables <- readr::read_tsv(argv$table_files, col_names=c("names", "files"), col_types = readr::cols())
+params <- list(tables=setNames(tables$files, tables$names), model=model_file)
 
 custom_render_markdown("data_model_report", argv$out_prefix, 
                        parameters=params)
