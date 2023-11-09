9ef34d890c37d007bdede9682cce0052b2af7b14
Alyna Khan <akhan345@gmail.com>
2023-11-09 19:15:58 +0000

    Added template main, child, grandchild Rmd files

--- pheno_qc/run_phenotype_qc.R
+++ pheno_qc/run_phenotype_qc.R
@@ -5,18 +5,14 @@ library(AnVIL)
 library(readr)
 
 # Get parameters
-argp <- arg_parser("validate")
+argp <- arg_parser("data_qc") # the name of the task in the wdl
 argp <- add_argument(argp, "--table_files", help="2-column tsv file with (table name, table tsv file)")
-argp <- add_argument(argp, "--model_file", help="json file with data model")
-argp <- add_argument(argp, "--use_existing_tables", flag=TRUE, help="for any tables in the data model but not included in table_files, read the existing table from the AnVIL workspace for QC")
-
-# read data model
-model <- json_to_dm(argv$model_file)
+argv <- parse(argp)
 
 # read tables
 table_files <- read_tsv(argv$table_files, col_names=c("names", "files"), col_types="cc")
-message("tables to validate:")
+message("tables to QC:")
 print(table_files$names)
 
-params <- list(tables=check_files, model=argv$model_file)
-pass <- custom_render_markdown("phenotype_data_qc", parameters=params)
+params <- list(tables=table_files)
+pass <- rmarkdown::render(output_file="main_qc.Rmd", input = "main_qc.Rmd", params=parameters, quiet=TRUE) 
