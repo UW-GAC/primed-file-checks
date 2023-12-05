bafc48b9e93288bb84a9900d12b15dea8eb3965e
Amy Watt <amywatt97@gmail.com>
2023-12-05 00:03:26 +0000

    added rmd render to Rscript

--- pheno_qc/run_qc.R
+++ pheno_qc/run_qc.R
@@ -16,10 +16,11 @@ argp <- add_argument(parser = argp,
 argv <- parse_args(parser = argp)
 
 # read tables
-table_files <- read_tsv(argv$filename, col_names=c("names", "files"), col_types="cc")
+# table_files <- read_tsv(argv$filename, col_names=c("names", "files"), col_types="cc")
+table_files <- read_tsv(argv$filename, col_names=TRUE, col_types="cc")
 message("tables to QC:")
 print(table_files$names)
 
 parameters <- list(tables=table_files)
 
-# pass <- rmarkdown::render(output_file="template_main_qc.Rmd", input = "template_main_qc.Rmd", params=parameters, quiet=TRUE) 
+pass <- rmarkdown::render(input = "template_main_qc.Rmd", params=parameters, quiet=TRUE)
