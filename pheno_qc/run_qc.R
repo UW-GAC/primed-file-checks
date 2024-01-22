6313c4fb2536a4e85965b3719e48f9ac9f8acffd
Amy Watt <amywatt97@gmail.com>
2024-01-22 20:17:04 +0000

    added parameter for filepath

--- pheno_qc/run_qc.R
+++ pheno_qc/run_qc.R
@@ -5,7 +5,7 @@ library(AnVIL)
 library(readr)
 
 # setwd('/home/rstudio/primed-file-checks/pheno_qc')
-# Rscript run_qc.R --filename data_qc.tsv
+# Rscript run_qc.R --filename data_qc.tsv --filepath /home/rstudio/primed-file-checks/pheno_qc/test_data/
 
 # Get parameters
 argp <- arg_parser(description = "data qc") 
@@ -14,14 +14,25 @@ argp <- add_argument(parser = argp,
                      type = "character", 
                      nargs = 1,
                      help="2-column tsv file with (table name, table tsv file)")
+argp <- add_argument(parser = argp, 
+                     arg = "--filepath", 
+                     type = "character", 
+                     nargs = 1,
+                     help="rmd filepath")
 argv <- parse_args(parser = argp)
 
 # read tables
-# table_files <- read_tsv(argv$filename, col_names=c("names", "files"), col_types="cc")
 table_files <- read_tsv(argv$filename, col_names=TRUE, col_types="cc")
+
+# read filepath 
+filepath <- argv$filepath
+
 message("tables to QC:")
 print(table_files$table_names)
 
-parameters <- list(tables=table_files)
+message("filepath:")
+print(filepath)
+
+parameters <- list(tables=table_files, path = filepath)
 
 rmarkdown::render(input = "template_main_qc.Rmd", params=parameters, quiet=TRUE)
