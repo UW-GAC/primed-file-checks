97a1edc8d08cdd135ccf31d6fab9bca5d12a230d
Amy Watt <amywatt97@gmail.com>
2024-02-01 06:20:01 +0000

    copying template file to working directory

--- pheno_qc/run_qc.R
+++ pheno_qc/run_qc.R
@@ -36,4 +36,7 @@ input <- paste0(path_to_rmd, "template_main_qc.Rmd")
 
 parameters <- list(tables=table_files, path = path_to_rmd)
 
-rmarkdown::render(input = input, params=parameters, quiet=TRUE)
+file.copy(file.path(path_to_rmd, "template_main_qc.Rmd"), "qc_report.Rmd")
+rmarkdown::render(input = "qc_report.Rmd", params=parameters, quiet=TRUE)
+
+# rmarkdown::render(input = input, params=parameters, quiet=TRUE)
