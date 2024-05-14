0fa34dfb7c841f0724871e33b19f615090a69684
Adrienne Stilp <amstilp@uw.edu>
2024-05-14 14:52:58 -0700

    Update hematology test data to match new limits in data model
    We added min/max values to the data model for the hematology table,
    so the test data needed to be updated to respect those limits.
    Otherwise, the validation will fail.

--- pheno_qc/test_data/test_files.R
+++ pheno_qc/test_data/test_files.R
@@ -6,7 +6,7 @@ n <- 20 # number of rows in test data
 set.seed(4)
 
 subject <- tibble(
-  subject_id = paste0("subject", 1:n), 
+  subject_id = paste0("subject", 1:n),
   age_at_obs=round(runif(n, 20, 80))
 )
 
@@ -15,7 +15,7 @@ cmqt_anthropometry <- tibble(
   age_at_obs=rep(subject$age_at_obs),
   visit=rep(1, n),
   height_1=rnorm(n, 500, 10), # height in cm
-  weight_1=rnorm(n, 400, 5), # weight in kg 
+  weight_1=rnorm(n, 400, 5), # weight in kg
   bmi_1=weight_1 / (height_1 / 100)^2, # bmi in km/m^2
   # waist_hip_ratio_1
 )
@@ -45,19 +45,19 @@ cmqt_hematology <- tibble(
   age_at_obs=rep(subject$age_at_obs),
   visit=rep(1, n),
   # rbc_1
-  hemoglobin_1=rnorm(n, 10, 20),
-  hematocrit_1=rnorm(n, 0.4, 0.4),
+  hemoglobin_1=runif(n, 0, 100),
+  hematocrit_1=runif(n, 0, 100),
   # mcv_1
   # mch_1
   # mchc_1
   # rdw_1
-  wbc_1=rnorm(n, 100, 100),
+  wbc_1=runif(n, 0, 1000),
   # basophil_count_1
   # eosinophil_count_1
   # lymphocyte_count_1
   # monocyte_count_1
   # neutrophil_count_1
-  platelet_count_1=rnorm(n, 800, 200)
+  platelet_count_1=runif(n, 0, 1000),
   # mean_platelet_volume_1
 )
 
